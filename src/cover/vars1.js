ffc38872a5e002b047b3d6d002daef11f271a3bc
developer <stevenmtune@gmail.com>
2020-07-15 13:29:46 -0700

    changes footer_locations pug include to footer_sections and improves extensibility

--- src/cover/vars1.js
+++ src/cover/vars1.js
@@ -3,75 +3,114 @@ module.exports = {
 
 	website_url: "#",
 	
-	logo_url: "https://www.chicagolakeshorehotel.com/wp-content/uploads/2018/01/clsh-white_340x73.png",
+	logo_url: 'https://www.thewindsorsuites.com/assets/svg/logo.svg',
 
 	menu: [
 		{
-			href: "https://www.chicagolakeshorehotel.com/",
-			title: "HOME"
+		  href: 'https://www.thewindsorsuites.com/our-hotel',
+		  title: 'Our Hotel',
+		  submenu: [
+		    {
+		      href: 'https://www.thewindsorsuites.com/our-hotel',
+		      title: 'Overview'  
+		    },
+		    {
+		      href: 'https://www.thewindsorsuites.com/our-hotel/faq',
+		      title: 'FAQ'
+		    }
+		  ]
 		},
 		{
-			href: "https://www.chicagolakeshorehotel.com/rooms/",
-			title: "ROOMS",
-			submenu: [
-				{
-					title: 'GALLERY',
-					href: 'https://www.chicagolakeshorehotel.com/gallery/'
-
-				},
-				{
-					title: 'STANDARD DOUBLE ROOM',
-					href: 'https://www.chicagolakeshorehotel.com/rooms/standard-double-room/'
-
-				},
-				{
-					title: 'STANDARD KING ROOM',
-					href: 'https://www.chicagolakeshorehotel.com/rooms/standard-king-room/'
-
-				}
-			]
+		  href: 'https://www.thewindsorsuites.com/suites',
+		  title: 'Suites',
+		  submenu: [
+		    {
+		      href: 'https://www.thewindsorsuites.com/suites',
+		      title: 'Overview'
+		    },
+		    {
+		      href: 'https://www.thewindsorsuites.com/suites/extended-stay',
+		      title: 'Extended Stay'
+		    }
+		  ]
 		},
 		{
-			title: "FOOD",
-			href: "#",
-			submenu: [
-				{
-					title: 'RESTAURANTS',
-					href: 'https://www.chicagolakeshorehotel.com/restaurants/'
-				},
-				{
-					title: 'BANQUETS',
-					href: 'https://www.chicagolakeshorehotel.com/catering/'
-				}
-			]
+		  href: 'https://www.thewindsorsuites.com/skyline-level',
+		  title: 'Skyline Level'
 		},
 		{
-			title: 'ATTRACTIONS',
-			href: 'https://www.chicagolakeshorehotel.com/attractions/'
+		  href: 'https://www.thewindsorsuites.com/suites/residences',
+		  title: 'Apartments'
 		},
 		{
-			title: 'BLOG',
-			href: 'https://www.chicagolakeshorehotel.com/blog/'
+		  href: 'https://www.thewindsorsuites.com/special-pkg',
+		  title: 'Specials'
 		},
 		{
-			title: 'CONTACT',
-			href: 'https://www.chicagolakeshorehotel.com/contact/'
+		  href: 'https://www.thewindsorsuites.com/meet-celebrate',
+		  title: 'Meetings &amp; Groups'
+		},
+		{
+		  href: 'https://www.thewindsorsuites.com/philly-at-a-distance',
+		  title: 'Philly at a Distance'
+		},
+		{
+		  href: 'https://www.thewindsorsuites.com/photos',
+		  title: 'Photos'
 		}
 	],
 
-	footer_locations: [
-		{
-			title: "HAMILTON PLAZA HOTEL & CONFERENCE CENTER",
-			address_line_1: "150 King Street East",
-			address_line_2: "Hamilton Ontario - L8N 1B2, Canada",
-			phone_text: "Hotel Front Desk: 1-905-528-3451",
-			fax_text: "Fax: 1-905-522-2281",
-			email_text: "Email: info@hamiltonplaza.ca"
-		}
+	footer_sections: [
+		[
+			{
+				type: 'text',
+				value: '1700 Benjamin Franklin Pkwy'
+			},
+			{
+				type: 'text',
+				value: 'Philadelphia, PA 19103'
+			},
+			{
+				type: 'anchor',
+				value: {
+					href: 'mailto:contactus@thewindsorsuites.com',
+					text: 'contactus@thewindsorsuites.com'
+				}
+			},
+			{
+				type: 'anchor',
+				value: {
+					href:'tel:2159815678',
+					text: '215-981-5678'
+				}
+			}
+		],
+		[
+			{
+				type: 'image',
+				value: 'https://www.thewindsorsuites.com//assets/svg/logo-footer.svg'
+			}
+		],
+		[
+			{
+				type: 'icon_with_anchor',
+				value: {
+					href: 'https://www.facebook.com/TheWindsorSuites/',
+					icon_class: 'fa fa-facebook'
+				}
+			},
+			{
+				type: 'icon_with_anchor',
+				value: {
+					href: 'https://www.instagram.com/thewindsorsuites/',
+					icon_class: 'fa fa-instagram'
+				}
+			}
+		]
 	],
-
 	copyright_lines: [
-		"&copy;2017 Hamilton Plaza Hotel & Conference Center"
+		'&copy; 2020 THE WINDSOR SUITES, ALL RIGHTS RESERVED',
+		'<a href="https://www.tambourine.com/services/">Hotel Website Design By Tambourine <svg xmlns="http://www.w3.org/2000/svg" width="21" height="25.8" viewBox="0 0 21 25.8"><path d="M10.9 17.6c1.2 1.4 2.4 2.9 3.5 4.3 1-.2 1.6-.3 2.8-.2-1 .9-2 1.7-3 2.6 0 0-5.6-3.5-5.7-3.5 1.1-1.1 2.2-2.4 2.4-3.2zM20 5.7c.8.3 1.2 1.1.9 1.8v.1c-.3.8-1.1 1.2-1.9 1l-.6.6c-.1.1-.1.1-.2.1v.1c.1.4 0 .8-.3 1.1-.2.3-.6.6-1 .6-.7.1-1.5-.2-1.7-.9H14.3v-.3c0-.6-.2-1.2-.5-1.8v.1c-.8 1.8-2.5 3.7-5.9 4 .9.2 2.1.3 3.1.3.1 1 0 5.8-5 9.5-.4.4-2 1.4-2 1.4s.8 1 1.4 2.3c-.6-.1-5.4-2.1-5.4-2.1s3.7-2.3 4.7-7c.6-2.7-.1-5.8-2.1-8C2 7.8 1.2 6.9.9 5.9c-.4-2 1.9-3.2 3.5-3.7 1.3-.4 1.3-1 1.1-1.5C7.3.7 6 2.6 6.9 3.4c.8.8 1.3 1.5 1.4 2.6.1.7 0 1.4-.4 2-.1.4-.6.8-1 1.2 2-.1 4.2-.8 5.9-2-.5-.4-1.1-.6-1.8-.7h-.3v-.3-.6c-.7-.4-1-1.1-.7-1.9.3-.8 1.2-1.3 2-1h.1l.2-.2c.2-.2.5-.4.7-.5-.1-.4 0-.8.3-1.1.2-.4.6-.6 1-.7.4-.1.9 0 1.2.2.3.2.6.6.7 1 .5.1 1.1.3 1.5.5.3-.3.7-.4 1.2-.4.4 0 .8.2 1.1.5.3.3.4.7.4 1.1 0 .4-.2.8-.6 1.1.1.4.2.9.2 1.5zm-4.8 3.6h.2c.9 0 1.7-.3 2.4-.9.7-.6 1.2-1.5 1.2-2.5.1-.9-.2-1.8-.8-2.6-.7-.8-1.7-1.2-2.7-1.2-.9 0-1.8.3-2.5 1-.7.6-1.1 1.4-1.2 2.3v.2l.2.1c1.6.5 2.9 1.9 3.2 3.6z"></path></svg></a>'
 	]
 }
 
