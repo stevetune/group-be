a753b899e1e3ec0471734cdb6df10f62b38c6af2
developer <stevenmtune@gmail.com>
2020-09-28 15:03:10 -0700

    removes footer_copyright.pug, adds menuSections() mixin to replace copyright section, moves mixins.pug out of includes folder

--- src/cover/vars1.js
+++ src/cover/vars1.js
@@ -60,7 +60,7 @@ module.exports = {
 		}
 	],
 
-	footer_sections: [
+	footer_section_1: [
 		[
 			{
 				type: 'text',
@@ -84,33 +84,38 @@ module.exports = {
 					text: '215-981-5678'
 				}
 			}
-		],
+		]
+	],
+	footer_section_2: [
 		[
 			{
-				type: 'image',
-				value: 'https://www.thewindsorsuites.com//assets/svg/logo-footer.svg'
+				type: 'list',
+				value: [
+					{
+						href: 'https://www.universityinncorvallis.com/reviews.html',
+						title: 'Reviews'
+					},
+					{
+						href: 'https://www.universityinncorvallis.com/location.html',
+						title: 'Location'
+					},
+					{
+						href: 'https://www.universityinncorvallis.com/policies.html',
+						title: 'Policies'
+					}
+				]
 			}
 		],
 		[
 			{
-				type: 'icon_with_anchor',
+				type: 'image_with_anchor',
 				value: {
-					href: 'https://www.facebook.com/TheWindsorSuites/',
-					icon_class: 'fa fa-facebook'
-				}
-			},
-			{
-				type: 'icon_with_anchor',
-				value: {
-					href: 'https://www.instagram.com/thewindsorsuites/',
-					icon_class: 'fa fa-instagram'
+					src: 'https://www.universityinncorvallis.com/assets/images/vizlly-icon.png',
+					href: 'https://www.vizlly.com/',
+					text: 'Powered By Vizlly'
 				}
 			}
 		]
-	],
-	copyright_lines: [
-		'&copy; 2020 THE WINDSOR SUITES, ALL RIGHTS RESERVED',
-		'<a href="https://www.tambourine.com/services/">Hotel Website Design By Tambourine <svg xmlns="http://www.w3.org/2000/svg" width="21" height="25.8" viewBox="0 0 21 25.8"><path d="M10.9 17.6c1.2 1.4 2.4 2.9 3.5 4.3 1-.2 1.6-.3 2.8-.2-1 .9-2 1.7-3 2.6 0 0-5.6-3.5-5.7-3.5 1.1-1.1 2.2-2.4 2.4-3.2zM20 5.7c.8.3 1.2 1.1.9 1.8v.1c-.3.8-1.1 1.2-1.9 1l-.6.6c-.1.1-.1.1-.2.1v.1c.1.4 0 .8-.3 1.1-.2.3-.6.6-1 .6-.7.1-1.5-.2-1.7-.9H14.3v-.3c0-.6-.2-1.2-.5-1.8v.1c-.8 1.8-2.5 3.7-5.9 4 .9.2 2.1.3 3.1.3.1 1 0 5.8-5 9.5-.4.4-2 1.4-2 1.4s.8 1 1.4 2.3c-.6-.1-5.4-2.1-5.4-2.1s3.7-2.3 4.7-7c.6-2.7-.1-5.8-2.1-8C2 7.8 1.2 6.9.9 5.9c-.4-2 1.9-3.2 3.5-3.7 1.3-.4 1.3-1 1.1-1.5C7.3.7 6 2.6 6.9 3.4c.8.8 1.3 1.5 1.4 2.6.1.7 0 1.4-.4 2-.1.4-.6.8-1 1.2 2-.1 4.2-.8 5.9-2-.5-.4-1.1-.6-1.8-.7h-.3v-.3-.6c-.7-.4-1-1.1-.7-1.9.3-.8 1.2-1.3 2-1h.1l.2-.2c.2-.2.5-.4.7-.5-.1-.4 0-.8.3-1.1.2-.4.6-.6 1-.7.4-.1.9 0 1.2.2.3.2.6.6.7 1 .5.1 1.1.3 1.5.5.3-.3.7-.4 1.2-.4.4 0 .8.2 1.1.5.3.3.4.7.4 1.1 0 .4-.2.8-.6 1.1.1.4.2.9.2 1.5zm-4.8 3.6h.2c.9 0 1.7-.3 2.4-.9.7-.6 1.2-1.5 1.2-2.5.1-.9-.2-1.8-.8-2.6-.7-.8-1.7-1.2-2.7-1.2-.9 0-1.8.3-2.5 1-.7.6-1.1 1.4-1.2 2.3v.2l.2.1c1.6.5 2.9 1.9 3.2 3.6z"></path></svg></a>'
 	]
 }
 
